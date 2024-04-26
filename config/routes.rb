1de338418f917152746cfdea5f08d85076439c1f
Keith Wong <keithwong@Keiths-MacBook-Air.local>
2024-04-27 02:39:39 +0800

    squash to conifg

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "/paperFiles", to: 'home#paperFiles', as: 'paperFiles'
+  get "/patagonia", to: 'home#patagonia', as: 'patagonia'
+  get "/acme", to: 'home#acme', as: 'acme'
+  post "/data", to: 'home#data', as: 'data'
+  get "/destroy", to: 'home#destroy', as: 'destroy'
 
   # Defines the root path route ("/")
   root "home#index"
